5114bbad790817b5024ee80bfb0b1bfd36285670
RebekaSzigeti <116110563+RebekaSzigeti@users.noreply.github.com>
2023-12-04 19:54:44 +0200

    atszallas

--- main.cpp
+++ main.cpp
@@ -4,115 +4,78 @@
 
 using namespace std;
 
-vector<int> lehetosegek;
-
-
-
-void szam(int v[], int n){
-
+int main()
+{
+    vector<int> v;
+    ifstream in("input.txt");
+    ofstream out("output.txt");
+    int n,m,x,y,nr=0;
+    in >> n >> m;
+    int indul[n], vegez[n], sorrend[n];
+    for(int i=0;i<n;i++){
+        in >> x;
+        indul[i] = x;
+        in >> x;
+        vegez[i]=x;
+    }
+    for(int i=0;i<n;i++){
+        sorrend[i]=i+1;
+    }
     int aux;
     for(int i=0;i<n;i++){
         for(int j=i+1;j<n;j++){
-            if(v[i]>v[j]){
-                aux = v[i];
-                v[i]= v[j];
-                v[j] = aux;
-            }
+           if( vegez[i]<vegez[j]){
+                aux = vegez[i];
+                vegez[i]=vegez[j];
+                vegez[j]=aux;
+
+                aux = sorrend[i];
+                sorrend[i]=sorrend[j];
+                sorrend[j]=aux;
+
+                aux = indul[i];
+                indul[i]=indul[j];
+                indul[j]=aux;
+
+
+           }
+
         }
     }
 
-    for(int i=0;i<n;i++){
-        if(v[i]<0){
-            v[i]= abs(v[i]);
-        }
-    }
+    y=1;
+    int prev_y;
 
-    int k=1, us =0;
-    for(int i=0;i<n;i++){
-        us = us*k+v[i];
-        k=k*10;
-    }
-
-    int l=true;
-
-
-    for(int j=0;j<lehetosegek.size();j++){
-            if(lehetosegek[j] == us){
-                l = false;
+    while(y!=m){
+         prev_y=y;
+        for(int i=0;i<n;i++){
+            if(y>=indul[i]){
+                y = vegez[i];
+                v.push_back(sorrend[i]);
+                nr++;
                 break;
             }
         }
-
-        if(l){
-            lehetosegek.push_back(us);
-            cout << us << " ";
-        }
-
-    return ;
-}
-
-
-void backtrack(int v[], int n, int start){
-    if(start == n){
-        return;
-    }
-
-    for(int i=start;i<n;i++){
-        int tempv[n];
-        for(int j=0;j<n;j++){
-            tempv[j]=v[j];
-        }
-        tempv[i]=tempv[i]*(-1);
-        int tempstart = start+1;
-        szam(tempv,n);
-
-
-        backtrack(tempv,n,tempstart);
-
-    }
-
-    return;
-
-}
-
-int main()
-{
-    ifstream in("input.txt");
-    int n;
-    in >> n;
-    int v[n];
-    for(int i=0;i<n;i++){
-        in >> v[i];
-    }
-
-    int aux;
-    for(int i=0;i<n;i++){
-        for(int j=i+1;j<n;j++){
-            if(v[i]>v[j]){
-                aux = v[i];
-                v[i]= v[j];
-                v[j] = aux;
-            }
+        if(prev_y==y){
+            break;
         }
     }
 
-    for(int i=0;i<n;i++){
-        if(v[i]<0){
-            v[i]= abs(v[i]);
+    if(y==prev_y){
+        cout << -1;
+    } else {
+        cout << nr-1 << endl;
+        for(int i=0;i<v.size();i++){
+            cout << v[i] << endl;
         }
+
     }
 
-    int k=1, us =0;
-    for(int i=0;i<n;i++){
-        us = us*k+v[i];
-        k=k*10;
-    }
-
-    lehetosegek.push_back(us);
 
 
-    backtrack(v,n,0);
-    cout << lehetosegek.size();
+
+
+
 
     return 0;
 }
