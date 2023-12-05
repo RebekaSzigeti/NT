1150e0281fc19ede6c050b7b4e100d125df67272
RebekaSzigeti <116110563+RebekaSzigeti@users.noreply.github.com>
2023-12-05 20:46:29 +0200

    sipalya

---
+++ sipalya.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main()
+{
+    ifstream in("input.txt");
+    int n, k;
+    in >> n >>k;
+    int v[n];
+    for(int i=0;i<n;i++){
+        in >> v[i];
+    }
+
+    int s =0,y=0, mn,mx, cnt = 0;;
+    int p[k];
+
+    for(int i=0;i<n-k+1;i++){
+        s=0;
+        y=0;
+        cnt = 0;
+        for(int j=i;j<i+k;j++){
+            p[y] = v[i];
+            y++;
+        }
+        mx = 0;
+        for(int j=1;j<k;j++){
+            if( p[j] > p[0]-j){
+                if(mx == 0){
+                    mx = p[j]+j;
+                } else {
+                    if(p[j]+j> mx){
+                        mx = p[j]+j+1;
+                    }
+                }
+            }
+        }
+        if(mx == 0){
+            for(int j=1;j<k;j++){
+                cnt = cnt + p[0]-j-p[j];
+
+            }
+
+
+        } else {
+            p[0] = mx;
+            for(int j=1;j<k;j++){
+                cnt = cnt + p[0]-j-p[j];
+
+
+            }
+
+        }
+        if(i==0){
+            mn = cnt;
+        } else {
+            if(cnt < mn){
+                mn = cnt;
+            }
+        }
+
+
+
+    }
+
+    cout << mn;
+
+
+    return 0;
+}
